b82d08b63cf0812cb7b7fd0413f076e392310813
selindoga <73192627+selindoga@users.noreply.github.com>
2022-11-20 00:33:05 +0300

    added base image for the gameplay

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(title: 'Tic tac toe game'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage("Assets/base.png")),
+        ),
+        //child: ElevatedButton(
+        //  onPressed: () {},
+        //  child: const Text("X"),
+        //),
+      ),
+    );
+  }
+}
